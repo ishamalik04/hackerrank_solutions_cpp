8d6a0c3db7a811a8b9fb925fa614462a48a1d08c
Isha Kumari <113428203+ishamalik04@users.noreply.github.com>
2023-05-17 17:37:15 +0530

    Add files via upload

---
+++ Array Reversal.cpp
@@ -0,0 +1,61 @@
+Given an array, of size , reverse it.
+
+Example: If array, , after reversing it, the array should be, .
+
+Input Format
+
+The first line contains an integer, , denoting the size of the array. The next line contains  space-separated integers denoting the elements of the array.
+
+Constraints
+
+
+, where  is the  element of the array.
+
+Output Format
+
+The output is handled by the code given in the editor, which would print the array.
+
+Sample Input 0
+
+6
+16 13 7 2 1 12
+Sample Output 0
+
+12 1 2 7 13 16
+Explanation 0
+
+Given array,  = . After reversing the array,  =
+
+Sample Input 1
+
+7
+1 13 15 20 12 13 2
+Sample Output 1
+
+2 13 12 20 15 13 1
+
+
+
+solution
+
+#include <stdlib.h>
+
+int main()
+{
+    int num, *arr, i;
+    scanf("%d", &num);
+    arr = (int*) malloc(num * sizeof(int));
+    for(i = 0; i < num; i++) {
+        scanf("%d", arr + i);
+    }
+
+
+ for(i=num-1;i>=0;i--)
+ {
+     printf("%d\t",*(arr+i));
+ }
+
+   /* for(i = 0; i < num; i++)
+        printf("%d ", *(arr + i));*/
+    return 0;
+}
